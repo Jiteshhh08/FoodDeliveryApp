afe95cae58287d9e0041a8543237faf9f60b3f34
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-02-04 22:32:45 +0530

    Added functionality for logIn btn

--- Functionality/Header.js
+++ Functionality/Header.js
@@ -2,9 +2,22 @@ const Header = document.querySelector('.headerContainer')
 const searchBar = document.querySelector('.searchBar')
 const searchBtn = document.querySelector('.searchBtn')
 const searchSVG = document.querySelector('.searchBtnSVG')
+const logIn = document.querySelector('.logIn')
+const logInPage = document.querySelector('.logInPage')
+const closeLogIn = document.querySelector('.closeLogInPage')
  
 searchBtn.addEventListener('click', () => {
     searchBar.classList.remove('hidden')
     searchBtn.classList.add('hidden')
     searchSVG.classList.remove('hidden')
+})
+
+logIn.addEventListener('click', () => {
+    logInPage.classList.remove('hidden')
+    closeLogIn.classList.remove('hidden')
+})
+
+closeLogIn.addEventListener('click', () => {
+    logInPage.classList.add('hidden')
+    closeLogIn.classList.add('hidden')
 })
