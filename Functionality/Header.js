4d5a05ea788d2f2baa1fb7b95ab4868d48a6f0c3
Jitesh Jha <jitesh.jha2008@gmail.com>
2026-02-05 22:13:55 +0530

    Added a contact us btn

--- Functionality/Header.js
+++ Functionality/Header.js
@@ -12,6 +12,14 @@ searchBtn.addEventListener('click', () => {
     searchSVG.classList.remove('hidden')
 })
 
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'Escape') {
+        searchBar.classList.add('hidden')
+        searchSVG.classList.add('hidden')
+        searchBtn.classList.remove('hidden')
+    }
+})
+
 logIn.addEventListener('click', () => {
     logInPage.classList.remove('hidden')
     closeLogIn.classList.remove('hidden')
@@ -20,4 +28,19 @@ logIn.addEventListener('click', () => {
 closeLogIn.addEventListener('click', () => {
     logInPage.classList.add('hidden')
     closeLogIn.classList.add('hidden')
+})
+
+
+//Click outside the searchBar
+
+searchBar.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+
+document.addEventListener('click', (e) => {
+    if (!Header.contains(e.target)) {
+        searchBar.classList.add('hidden')
+        searchBtn.classList.remove('hidden')
+        searchSVG.classList.add('hidden')
+    }
 })
